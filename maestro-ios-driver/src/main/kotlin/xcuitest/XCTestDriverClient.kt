08b209cff279f38cf11a9fb4f0c8619234351f08
Leland Takamine <leland.takamine@gmail.com>
2025-02-14 07:08:46 -0800

    Gracefully handle case where XCTestDriver error cannot be parsed (#2312)

--- maestro-ios-driver/src/main/kotlin/xcuitest/XCTestDriverClient.kt
+++ maestro-ios-driver/src/main/kotlin/xcuitest/XCTestDriverClient.kt
@@ -1,5 +1,6 @@
 package xcuitest
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import hierarchy.ViewHierarchy
 import maestro.utils.HttpClient
@@ -244,8 +245,12 @@ class XCTestDriverClient(
         pathString: String,
         responseBodyAsString: String,
     ): String {
-        logger.warn("Status code: $code, body: $responseBodyAsString");
-        val error = mapper.readValue(responseBodyAsString, Error::class.java)
+        logger.warn("XCTestDriver request failed. Status code: $code, path: $pathString, body: $responseBodyAsString");
+        val error = try {
+            mapper.readValue(responseBodyAsString, Error::class.java)
+        } catch (_: JsonProcessingException) {
+            Error("Unable to parse error", "unknown")
+        }
         when {
             code in 400..499 -> {
                 logger.error("Request for $pathString failed with bad request ${code}, body: $responseBodyAsString")
